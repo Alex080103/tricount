808523163ccf3e28aa139cd536ac96f182939e44
Theosch16 <schmitt1690@gmail.com>
2025-05-22 14:56:45 +0200

    add login front

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,6 +11,8 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  
+
   return (
     <Tabs
       screenOptions={{
@@ -29,7 +31,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Connexion',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
